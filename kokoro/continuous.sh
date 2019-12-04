eeb73240650b5970d42f24c38e4d89450aacb2df
Chanseok Oh <chanseok@google.com>
2019-12-04 17:45:01 -0500

    test

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -5,16 +5,12 @@ set -x
 
 gcloud components install docker-credential-gcr
 
-# For macOS to find docker-credential-gcr
-export PATH=$PATH:/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/
-
-docker-credential-gcr || true
-which docker-credential-gcr || true
-which docker-credential-osxkeychain
-
 # docker-credential-gcr uses GOOGLE_APPLICATION_CREDENTIALS as the credentials key file
 export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_KEYSTORE_DIR}/72743_jib_integration_testing_key
 docker-credential-gcr configure-docker
+cat ${HOME}/.docker/config.json || true
+
+exit 0
 
 # Stops any left-over containers.
 docker stop $(docker ps --all --quiet) || true
